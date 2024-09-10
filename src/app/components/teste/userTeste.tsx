52f55f6974ea3c81997eaeaa3e2246b69636ceb6
avinhasmarchetti@gmail.com <avinhasmarchetti@gmail.com>
2024-09-10 16:44:07 -0300

    Salvando mudanças locais antes de fazer o pull

--- src/app/components/teste/userTeste.tsx
+++ src/app/components/teste/userTeste.tsx
@@ -1,25 +1,107 @@
-import UserType from "@/app/services/user"
+'use client'
+import UserType, { removeUser } from "@/app/services/user"
 // import { saveUser, removeUser } from "@/app/services/user"
 import { useState } from "react"
 
+import { saveUser } from "@/app/services/user"
+
 type Props = {
     users: UserType[]
+    user: UserType
 }
 
-export default function UserTeste({ users }: Props) {
+export default function UserTeste({ users, user: novoUser }: Props) {
     console.log(users)
+    const [user, setUser] = useState<UserType>(novoUser)
     return (
         <>
-            <div className="text-white">
-                <h1>TESTE</h1>
-                {users.map((user) => (
-                    <div key={user.id}>
-                        <p>Nome: {user.nome}</p>
-                        <p>Email: {user.email}</p>
-                    </div>
-                ))}
-            </div>
 
+            <section className="w-full flex flex-row h-full">
+
+
+                <div className="flex flex-col bg-green-600 w-6/12 text-white h-44">
+
+                    <form action={saveUser} className="flex flex-col w-6/12 text-center text-black m-auto justify-between h-full">
+
+                        <h1>TESTE</h1>
+
+                        <input type="hidden" name="id" value={`${user?.id}`} />
+                        <input
+
+                            type="text"
+
+                            name="nome"
+
+                            placeholder="Seu nome..."
+
+                            value={user.nome}
+
+                            onChange={(e) => setUser({ ...user, nome: e.target.value })}
+
+                        />
+
+                        <input
+
+                            type="text"
+
+                            name="email"
+
+                            placeholder="Seu Email..."
+
+                            value={user.email}
+
+                            onChange={(e) => setUser({ ...user, email: e.target.value })}
+
+                        />
+
+                        <input
+
+                            type="password"
+
+                            name="senha"
+
+                            placeholder="Sua Senha..."
+
+                            value={user.senha}
+
+                            onChange={(e) => setUser({ ...user, senha: e.target.value })}
+
+                        />
+
+
+                        <button>Cadastrar</button>
+
+                    </form>
+
+                </div>
+
+
+
+                <div className="bg-yellow-600 w-6/12 flex flex-col ">
+
+                    {users.map((user) => (
+
+                        <div key={user.id} className="flex flex-col m-auto w-10/12 bg-black text-white h-max">
+
+                            <p>Nome: {user.nome}</p>
+
+                            <p>Email: {user.email}</p>
+
+                            <p>Senha: {user.senha}</p>
+
+                            <button onClick={() => removeUser(user)}>Delete</button>
+                            <button onClick={() => setUser(user)}>Editar</button>
+                        </div>
+
+                    ))}
+
+                </div>
+
+
+
+
+
+            </section>
         </>
     )
 }
--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -10,14 +10,15 @@ type UserType = {
     nome: string
     email: string
     senha: string
-    resetPasswordToken: string | null
 }
 
 export default UserType
 
-// export async function getEmptyUser(): Promise<UserType> {
-//     return { id: null, nome: "", email: "", senha: "", resetPasswordToken: "" }
-// }
+
+export async function getEmptyUser(): Promise<UserType> {
+    return { id: null, nome: "", email: "", senha: "" }
+}
+
 export async function getUsers(): Promise<UserType[]> {
     try {
         return await db.execute<UserType>(sql`SELECT * FROM "user"`);
@@ -27,33 +28,72 @@ export async function getUsers(): Promise<UserType[]> {
     }
 }
 
-// export async function saveUser(formData: FormData) {
-//     const id = +(formData.get('id') as string) as number
-//     const nome = formData.get('nome') as string
-//     const email = formData.get('email') as string
-//     const senha = formData.get('senha') as string
-//     const resetPasswordToken  = formData.get('resetPasswordToken') as string
+export async function saveUser(formData: FormData) {
 
-//     const user: UserType = {
-//         id,
-//         nome,
-//         email,
-//         senha,
-//         resetPasswordToken  
-//     }
+    const id = +(formData.get('id') as string) as number
+    const nome = formData.get('nome') as string
+    const email = formData.get('email') as string
+    const senha = formData.get('senha') as string
 
-//     if(!id){
-//         await db.execute(sql`INSERT INTO user (nome) VALUES (${user.nome})`)
-//     }else{
-//         await db.execute(sql`UPDATE user SET nome=${user.nome} WHERE id=${user.id}`)
-//     }
+    if (!nome || !email || !senha) {
+        throw new Error('Todos os campos (nome, email, senha) devem estar preenchidos.');
+    }
 
-//     redirect('/')
-// }
+    // Verificar se o email existe (formato básico de email)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        throw new Error('O email fornecido não é válido.');
+    }
 
-// export async function removeUser(user: UserType) {
+    const senhaRegex = /^(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{6,}$/;
+    if (!senhaRegex.test(senha)) {
+        throw new Error('A senha deve conter mais de 5 caracteres e pelo menos 1 caractere especial.');
+    }
 
-//     await db.execute(sql`DELETE FROM user WHERE id=${user.id}`)
+    const user: UserType = {
+        id,
+        nome,
+        email,
+        senha
+    }
 
-//     redirect('/')
-// }
+
+    if (!id) {
+        await db.execute(sql`INSERT INTO "user" (nome, email, senha) VALUES (${user.nome}, ${user.email}, ${user.senha} )`)
+    } else {
+        await db.execute(sql`UPDATE "user" SET nome=${user.nome}, email=${user.email}, senha=${user.senha} `)
+    }
+
+
+    redirect('/')
+}
+
+export async function removeUser(user: UserType) {
+    await db.execute(sql`DELETE FROM "user" WHERE id=${user.id}`)
+
+    redirect('/')
+}
+
+//LOGIN
+
+export async function loginUser(formData: FormData) {
+    const email = formData.get('email') as string;
+    const senha = formData.get('senha') as string;
+
+    if (!email || !senha) {
+        throw new Error('Email e senha são obrigatórios para o login.');
+    }
+
+    const [user] = await db.execute<UserType>(sql`SELECT * FROM "user" WHERE email = ${email}`);
+
+    if (!user) {
+        throw new Error('Usuário não encontrado.');
+    }
+
+    if (user.senha !== senha) {
+        throw new Error('Senha incorreta.');
+    }
+
+    //Página depois do login
+    redirect('/');
+}
