d43ea6a3f6c5eaa835a1201ca5d2463d2989e478
duduborges <duduborges333969@gmail.com>
2024-09-10 14:31:22 -0300

    atualizado a merda do request

---
+++ src/app/components/Navigation/navbar.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image'
+import logoNav from '../../../img/logo-completa-branca.png'
+import { MdDashboard } from "react-icons/md";
+import { IoMdNotifications } from "react-icons/io";
+import { FaGear } from "react-icons/fa6";
+interface navProps {
+    type: string
+}
+
+export default function Navbar({ type }: navProps) {
+    return (
+        <>
+            {type == "1" ? (
+                <>
+                    <header >
+                        <nav className=' flex items-center w-full justify-between px-10 '>
+                            <div className='w-[25%]'>
+                                <Image className='w-36' alt='Logo nav' src={logoNav} />
+                            </div>
+                            <div className='bg-[#2E2E2E] w-2/4 flex justify-evenly text-xl text-white p-6 rounded-full'>
+                                <a className=' transition- duration-300  hover:font-semibold' href="">Home</a>
+                                <a className=' transition- duration-300  hover:font-semibold' href="">Planos</a>
+                                <a className=' transition- duration-300  hover:font-semibold' href="">Sobre nós</a>
+                                <a className=' transition- duration-300  hover:font-semibold' href="">Contato</a>
+                            </div>
+                            <div className='w-[25%] flex justify-end gap-2 '>
+                                <button className='bg-[#F6CF45] w-40 hover:bg-[#F1fF14] transition-all duration-300 h-11 px-9  text-lg rounded-full'>Cadastrar</button>
+                                <button className='border-[#F6CF45] border-solid text-white border w-40 transition-all  duration-300 hover:bg-[#F1aF45] h-11 px-9  text-lg rounded-full'>Entrar</button>
+                            </div>
+                        </nav>
+                    </header>
+                </>
+            ) : type == '2' ? (
+                <>
+                    <header>
+                        <nav className=' flex items-center w-full justify-between px-10 '>
+                            <div className='w-[25%] flex items-center'>
+                                <Image className='w-36' alt='Logo nav' src={logoNav} />
+                            </div>
+                            <div className='bg-[#2E2E2E] w-2/4 flex justify-evenly text-xl text-white p-6 rounded-full'>
+                                <a className=' flex items-center gap-2 transition-all duration-300   hover:font-bold' href=""><MdDashboard size={27} /> Departamentos</a>
+                                <a className=' flex items-center gap-2 transition-all duration-300   hover:font-bold' href=""><IoMdNotifications size={30} /> Notificações</a>
+                                <a className=' flex items-center gap-2 transition-all duration-300   hover:font-bold' href=""><FaGear size={25} /> Configurações</a>
+                            </div>
+                            <div className='w-[25%] flex justify-end gap-2 '>
+                                <div className=' w-14 h-14 rounded-full bg-white flex items-center justify-center text-xl'>A</div>
+                            </div>
+
+                        </nav>
+                    </header>
+                </>
+            ) : alert("Tipo de navbar invalido")}
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,41 @@
-import db from "./services/db";
-import UserTeste from "./components/teste/userTeste";
-import { getUsers } from "@/app/services/user";
 
-export const dynamic = "force-dynamic"
+import Navbar from "@/app/components/Navigation/navbar";
+import Image from "next/image";
+import chart from "../img/chartsPng 1.png"
+import { FaCalculator } from "react-icons/fa6";
+import { BsFillClockFill } from "react-icons/bs";
+import { MdOutlineAttachMoney } from "react-icons/md";
+import { RiBarChartFill } from "react-icons/ri";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { IoExtensionPuzzle } from "react-icons/io5";
 
-export default async function Home() {
-  const users = await getUsers();
-  // const user = await getEmptyUser();
 
+export default function Home() {
 
-  return <UserTeste users={users}/>
+  return (
+    <>
+      <main className="flex flex-col fundoHome w-full min-h-screen">
+        <Navbar type="1" />
+        <section>
+          <div className="flex w-10/12 mt-20 m-auto justify-center items-start">
+            <div className="w-1/2 flex  flex-col">
+              <h1 className="text-5xl font-semibold text-white w-full">
+                Gerencie seu patrimônio da forma mais eficiente do mercado
+              </h1>
+              <p className="text-white font-light border-solid border-l-2 pl-4 w-4/6 my-5 text-xl border-[#F6CF45]">
+                Gestão Estratégica de Ativos: Monitorando, Avaliando e Otimizando Seu Patrimônio Empresarial
+              </p>
+              <button className="py-3 mt-5 text-xl w-2/4 bg-[#F6CF45] rounded-full">
+                Conheça nosso serviço
+              </button>
+            </div>
+            <div className="w-1/2 m-auto flex justify-end">
+              <Image className="h-[550px] w-[721px]" src={chart} alt="" />
+            </div>
+          </div>
+
+        </section>
+      </main>
+    </>
+  );
 }
