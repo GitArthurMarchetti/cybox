20a3f549ebf566b5178e43c8586547d75aa97a03
avinhasmarchetti@gmail.com <avinhasmarchetti@gmail.com>
2024-10-20 14:16:05 -0300

    Resolvendo conflitos e incluindo alterações no socialLogin.tsx

--- src/app/loginTeste/socialLogin.tsx
+++ src/app/loginTeste/socialLogin.tsx
@@ -4,8 +4,8 @@ import { doSocialLogin } from "../services/login"
 
 export default function DoSocialLogin() {
     return (
-            <form action={doSocialLogin}>
-                <button type="submit" name="action" value="google">Sign in with Google</button>
+            <form >
+                <button >Sign in with Google</button>
             </form>
     )
 }
